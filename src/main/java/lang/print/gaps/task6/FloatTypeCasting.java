1e2f558ad6a0748310903074ec79f40c654590f4
Mirzonabot <mirzonabot.mirzonabotov_2023@ucentralasia.org>
2023-07-17 19:35:18 +0600

    solved

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,13 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        int roundedNumber;
+        if (numberToBeRounded - (int) numberToBeRounded >= 0.5) {
+            roundedNumber = (int) numberToBeRounded + 1;
+        } else {
+            roundedNumber = (int) numberToBeRounded;
+        }
+        System.out.println(roundedNumber);
+
     }
 }
